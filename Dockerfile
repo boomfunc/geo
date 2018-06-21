67185f24bd3599966d981565faf6d3e4f411661c
Alexander Gurinov <alexander.gurinov@gmail.com>
2018-06-21 16:51:32 +0300

    add python script

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,20 @@ RUN mkdir -p /bmp
 WORKDIR /bmp
 
 ADD . /bmp
+
+# custom additional actions
+ENV PYGEOIP_VERSION 0.3.2
+
+RUN set -ex \
+		&& go get -v -d ./... \
+		&& go install -v ./... \
+		\
+		&& mv /go/src/app/docker-entrypoint.sh /usr/local/bin \
+		&& chmod +x /usr/local/bin/docker-entrypoint.sh
+
+
+RUN set -ex \
+		&& curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py \
+		&& python /tmp/get-pip.py \
+		\
+		&& pip install --no-cache-dir pygeoip==${PYGEOIP_VERSION}
--- geoip.py
+++ geoip.py
@@ -1,1 +1,25 @@
-print 'GEOIP WORKS'
+import os
+import sys
+
+import pygeoip
+
+
+def get_geo_record(ip):
+	try:
+		# get geo database and
+		# get record by ip address ( dict )
+		geo_record = pygeoip.GeoIP(
+			os.path.join(
+				os.getcwd(),
+				'geoip_city.dat'
+			)
+		).record_by_addr(ip)
+	except (pygeoip.GeoIPError, IOError, AttributeError):
+		geo_record = None
+
+	return geo_record or {}
+
+
+if __name__ == '__main__':
+	ip = sys.stdin.read()
+	sys.stdout.write('NDJKNDJKDNJKDNDJKNDJKNDJKDNJKDNDJKNDJKDNJKDNDNJK', ip)
